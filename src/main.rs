54ca8f3c4734c09de4026cabd51d5ef918d1fcdd
Makar Lill <lill.makar.vik@gmail.com>
2025-10-21 10:12:55 +0300

    add logic in main.rs (feature)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
+use std::io;
+use std::fs;
+use std::path::Path;
 
+fn main() {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+    let path = input.trim();
+
+    if !Path::new(path).exists() {
+        println!("failure");
+        return;
+    }
+
+    match fs::read_to_string(path) {
+        Ok(_) => println!("success"),
+        Err(e) => {
+            if e.kind() == io::ErrorKind::PermissionDenied {
+                println!("success");
+            } else {
+                println!("failure");
+            }
+        }
+    }
 }
