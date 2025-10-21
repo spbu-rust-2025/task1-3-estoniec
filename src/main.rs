6a1b32bdbb75dcf92425d5075243e0eb1ffc7dba
Makar Lill <lill.makar.vik@gmail.com>
2025-10-21 11:49:29 +0300

    update logic in main.rs (fix)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::io;
 use std::fs;
-use std::path::Path;
+use std::io;
 
 fn main() {
     let mut input = String::new();
@@ -8,19 +7,8 @@ fn main() {
     io::stdin().read_line(&mut input).unwrap();
     let path = input.trim();
 
-    if !Path::new(path).exists() {
-        println!("failure");
-        return;
-    }
-
     match fs::read_to_string(path) {
         Ok(_) => println!("success"),
-        Err(e) => {
-            if e.kind() == io::ErrorKind::PermissionDenied {
-                println!("success");
-            } else {
-                println!("failure");
-            }
-        }
+        Err(_) => println!("failure"),
     }
 }
